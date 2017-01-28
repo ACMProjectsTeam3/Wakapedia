f2caf11a745a620fda715fee56a0f9db958a1694
Gerard Geer <gerarddgeer@gmail.com>
2017-01-27 19:47:02 -0600

    Added comments. Elizabeth comment your code

--- Bundle.py
+++ Bundle.py
@@ -1,12 +1,31 @@
+"""
+This encapsulate all the components of a website.
+"""
 class Site(object):
     def __init__(self, html, CSS, js):
+		
+		# This is the html. It's a single string.
         self.html = html
+		# The CSS is an array of strings that represent potentially
+		# multiple css files.
         self.CSS = CSS
+		# This is also a list of strings that represent potentially
+		# multiple css files.
         self.js = js
 
+"""
+This is the object that we pass to each component (the scraper, MC engine,
+reconstructor, etc). Modify what your component needs to change, then
+pass it down the line. Imagine an assembly line.
+"""
 class Bundle(object):
     def __init__(self, txt, link, site):
+		# This is a list of strings, where each string is the contents of
+		# a paragraph.
         self.text = txt
+		# This is the URL of the page we need to get. It's a string.
         self.URL = link
+		# This is an instance of Site, and is what we need to send back to the
+		# user. This contains the HTML that we're modifying.
         self.site = site
 
