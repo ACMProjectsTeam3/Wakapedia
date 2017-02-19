c5dad4474783109094e8864a940fe44b28a8ea22
Arpakasso <etrinh96@gmail.com>
2017-02-18 18:38:24 -0600

    ThreadedCategoryScraper.py added multithreading

--- HTMLscraper.py
+++ HTMLscraper.py
@@ -1,6 +1,6 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 from Bundle import Site, Bundle
-import urllib, re
+import urllib, re, time
 
 """
 This gets the texts in paragraphs of a webpage given a Bundle object
@@ -11,8 +11,14 @@ Returns Bundle object after updating text and HTML fields
 def scrape(bun):
     ### opens url so it's like a file
   link = urllib.request.urlopen(bun.URL)
-    ### instantiate BeautifulSoup object
-  soup = BeautifulSoup(link, 'lxml')
+  soup = None
+    ### flag for retrieving categories (or not)
+  if bun.categories:
+    soup = BeautifulSoup(link, 'lxml')
+  else:
+    p_tags = SoupStrainer('p')
+    soup = BeautifulSoup(link, 'lxml', parse_only=p_tags)
+  
     ### dictionary of paragraphs
   doc = {}
     ### add token and count to replace paragraphs in HTML
@@ -28,30 +34,28 @@ def scrape(bun):
     alltxt = alltxt + para.get_text()
       ### replace <p> contents with a token
     para.string = token + str(count)
-      ### increment the token
     count+=1
- 
+
     ### get the list of categories
   cats = []
-  for cat in soup.find('div', {'id': 'catlinks'}).find('ul').findAll('li'):
-      cats.append(cat.get_text())
-
-    ### update Bundle with raw text
+  if bun.categories:
+    for cat in soup.find('div', {'id': 'catlinks'}).find('ul').findAll('li'):
+      cats.append('https://en.wikipedia.org' + cat.find('a')['href'])
+  
+    ### update stuff in Bundle
   bun.paragraphs = doc
   bun.text = alltxt
-    ### update Bundle with tokened html
   bun.site.html = str(soup)
-    ### update Bundle with categories
   bun.categories = cats
-    ### retrieve CSS file(s) links and update Bundle
+    ### retrieve CSS file(s) links
   bun.site.CSS = soup.find_all('link', rel='stylesheet')
-    ### retrieve js file(s) links and update Bundle
+    ### retrieve js file(s) links
   bun.site.js = soup.find_all('script', src=re.compile('.*'))
   return bun
 
-x = Bundle('', '', 'https://en.wikipedia.org/wiki/Alpaca', Site('','',''), [])
+"""
+x = Bundle(None, None, 'https://en.wikipedia.org/wiki/Alpaca', Site('','',''), None)
 x = scrape(x)
-print (x.site.CSS)
-print (x.site.js)
-print (x.categories)
 
+print (x.categories)
+"""
---
+++ ThreadedCategoryScraper.py
@@ -0,0 +1,71 @@
+from Bundle import Site, Bundle
+from bs4 import BeautifulSoup, SoupStrainer
+from HTMLscraper import scrape
+import urllib, sys, threading
+
+class catThread(threading.Thread):
+    def __init__(self, url):
+        threading.Thread.__init__(self)
+        self.url = url
+    def run(self):
+        scrape_category_page(self.url)
+
+lock = threading.Lock()
+
+"""
+This gets the text from all of the wikipedia pages
+of all of the categories in thedata between threads bundle. Returns a string.
+"""
+def scrape_category(bun):
+    global ALL_TEXT, non_bmp_map, threads, count
+    ALL_TEXT = ''
+    count = 1
+    
+      ### maps chars that the python terminal doesn't like to chars it does like
+    non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
+
+      ### process each category in a new thread
+    threads = []
+    for link in bun.categories:
+        try:
+            t = catThread(link)
+              ### kills the baby thread if momma thread dies
+            t.daemon = True
+            t.start()
+            threads.append(t)
+        except:
+            print ("Error: Unable to thread.")
+            
+      ### wait for all the threads to finish so the main thread doesn't just nope out
+    for thr in threads:
+        thr.join()
+    
+    return ALL_TEXT
+
+"""
+This gets links to wikipedia pages from a category page.
+Returns a list.
+"""
+def scrape_category_page(url):
+    global ALL_TEXT, non_bmp_map, threads, count
+    soup = BeautifulSoup(urllib.request.urlopen(url), 'lxml', parse_only=SoupStrainer('div'))
+
+      ### accounts for categories with over 200 pages
+    link = soup.find('a', href=True, text='next page')
+    if (link != None):
+        try:
+            t = catThread('https://en.wikipedia.org' + link['href'])
+            t.daemon = True
+            t.start()
+            threads.append(t)
+        except:
+            print ("Error: Unable to thread.")
+
+      ### sends links of wikipedia articles in the category to be scraped
+    pages_in_category = soup.find('div', {'id':'mw-pages'}).find('div',{'class':'mw-category'})
+    for obj in pages_in_category.findAll('a'):
+        tempbun = scrape(Bundle('', '', 'https://en.wikipedia.org' + obj['href'], Site('', '', ''), False))
+        with lock:
+            ALL_TEXT += tempbun.text.translate(non_bmp_map)
+            print (count)
+            count += 1    
