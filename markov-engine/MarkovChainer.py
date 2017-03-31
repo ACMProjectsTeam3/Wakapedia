76f142e039b3fcf113e2e6071cb4d27e53874500
Abhishaike <abhishaike@gmail.com>
2017-03-31 00:01:17 -0500

    Update MarkovChainer.py

--- markov-engine/MarkovChainer.py
+++ markov-engine/MarkovChainer.py
@@ -14,19 +14,19 @@ from random import randint
 def CreateSentences(Bundle): #definition to generate text. Firstword = word.replace(letter,"!") parameter is the file-path to the .txt file you'll be using to train the model, the second parameter is how many sentences you want out of the markov model.
 	FILE_PATH_OF_OLDSTUFF = None 
 	NEW_MODEL = None 
-		for x in Bundle.categories:
-			FILE_PATH_OF_OLDSTUFF = x
-			if (FILE_PATH_OF_OLDSTUFF == None):	
-				continue
-			FILE_PATH_OF_OLDSTUFF = FILE_PATH_OF_OLDSTUFF[FILE_PATH_OF_OLDSTUFF.find("Category:") + 9:]
-			FILE_PATH_OF_OLDSTUFF = "Categories/%s.mc" % FILE_PATH_OF_OLDSTUFF
-			for letter in FILE_PATH_OF_OLDSTUFF:
-			if letter == '_':
-				FILE_PATH_OF_OLDSTUFF = FILE_PATH_OF_OLDSTUFF.replace(letter, ' ')
-			with open(FILE_PATH_OF_OLDSTUFF) as json_file:  
-				model2_json = json.load(json_file)
-			OLD_MODEL = markovify.Text.from_json(model2_json)
-			NEW_MODEL = markovify.combine(OLD_MODEL, NEW_MODEL)
+	for x in Bundle.categories:
+		FILE_PATH_OF_OLDSTUFF = x
+		if (FILE_PATH_OF_OLDSTUFF == None):	
+			continue
+		FILE_PATH_OF_OLDSTUFF = FILE_PATH_OF_OLDSTUFF[FILE_PATH_OF_OLDSTUFF.find("Category:") + 9:]
+		FILE_PATH_OF_OLDSTUFF = "Categories/%s.mc" % FILE_PATH_OF_OLDSTUFF
+		for letter in FILE_PATH_OF_OLDSTUFF:
+		if letter == '_':
+			FILE_PATH_OF_OLDSTUFF = FILE_PATH_OF_OLDSTUFF.replace(letter, ' ')
+		with open(FILE_PATH_OF_OLDSTUFF) as json_file:  
+			model2_json = json.load(json_file)
+		OLD_MODEL = markovify.Text.from_json(model2_json)
+		NEW_MODEL = markovify.combine(OLD_MODEL, NEW_MODEL)
 	tool = language_check.LanguageTool('en-GB')
 	Text = ""
 	paragraphText = ""
