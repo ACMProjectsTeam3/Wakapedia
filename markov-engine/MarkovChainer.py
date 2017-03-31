a562c3f5c2d883e2c6eaefcc65ab77cb17506774
Abhishaike <abhishaike@gmail.com>
2017-03-30 21:20:07 -0500

    Update MarkovChainer.py

--- markov-engine/MarkovChainer.py
+++ markov-engine/MarkovChainer.py
@@ -5,6 +5,20 @@ import re
 import language_check
 import random
 import json
+import os
+import random
+from random import randint
+
+
+class POSifiedText(markovify.Text):
+    def word_split(self, sentence):
+        words = re.split(self.word_split_pattern, sentence)
+        words = [ "::".join(tag) for tag in nltk.pos_tag(words) ]
+        return words
+    def word_join(self, words):
+        sentence = " ".join(word.split("::")[0] for word in words)
+        return sentence
+    
 
 class EditedTextClass(POSifiedText):
     def __init__(self, input_text, state_size=2, chain=None, runs=None):
@@ -30,44 +44,39 @@ class EditedTextClass(POSifiedText):
         )
 
 
-class POSifiedText(markovify.Text):
-    def word_split(self, sentence):
-        words = re.split(self.word_split_pattern, sentence)
-        words = [ "::".join(tag) for tag in nltk.pos_tag(words) ]
-        return words
-    def word_join(self, words):
-        sentence = " ".join(word.split("::")[0] for word in words)
-        return sentence
 
-
-def CreateSentences(FILE_PATH_OF_INPUT, FILE_PATH_OF_OLDSTUFF, NUMSENTENCES): #definition to generate text. First parameter is the file-path to the .txt file you'll be using to train the model, the second parameter is how many sentences you want out of the markov model.
-	with open(FILE_PATH_OF_INPUT) as f:
-	    text = f.read() #open text file
-	FormattedText = re.sub( '\s+', ' ', text ).strip() #gonna be honest, I have zero clue what this does
-	NEW_MODEL = EditedTextClass(FormattedText, state_size = 4) #creates a markov model (using POS) from the formatted test of state_size 4
+def CreateSentences(Bundle): #definition to generate text. First parameter is the file-path to the .txt file you'll be using to train the model, the second parameter is how many sentences you want out of the markov model.
+	FILE_PATH_OF_OLDSTUFF = random.choice(Bundle.categories)
+	FILE_PATH_OF_OLDSTUFF = "Categories/%s.mc" % FILE_PATH_OF_OLDSTUFF
 	with open(FILE_PATH_OF_OLDSTUFF) as json_file:  
 		model2_json = json.load(json_file)
-	OLD_MODEL = EditedTextClass.from_json(model2_json)
+	NEW_MODEL = EditedTextClass.from_json(model2_json)
 	tool = language_check.LanguageTool('en-GB')
-	text = ""
-	NEW_MODEL = markovify.combine([NEW_MODEL, OLD_MODEL], [ .80, .20 ])
-	for i in range(NUMSENTENCES): #creates 'NUMSENTENCES' sentence, where NUMSENTENCES is an integer
-		text = NEW_MODEL.make_sentence(tries = 1) #this, along with the next while loop, basically just forces the markov model to try an infinite number of times to have SOMETHING come out. 
-		while (text == None):
-			text = NEW_MODEL.make_sentence(tries = 1)
-		matches = tool.check(text) #checks the grammar of the generated text
-		text = language_check.correct(text, matches) #corrects any mistakes the grammar checker found in the text
-		print (" ", end = "")
-		print (text, end="") #prints text. The 'end' is just there to ensure that no new line is created
-		NewLine = random.randint(0,30) #there is a 10% chance that a new line is created every time a sentence is created. This just makes the generated text not a huge block, and a little more natural looking 
-		if NewLine == 1 or NewLine == 2 or NewLine == 3:
-			print ("\n")
+	Text = ""
+	paragraphText = ""
+	for key in Bundle.paragraphs:
+		NumberOfSentences = random.randint(1,15)
+		for i in range(NumberOfSentences): #creates 'NUMSENTENCES' sentence, where NUMSENTENCES is an integer
+			Text = NEW_MODEL.make_sentence(tries = 1) #this, along with the next while loop, basically just forces the markov model to try an infinite number of times to have SOMETHING come out. 
+			while (Text == None): 
+				Text = NEW_MODEL.make_sentence(tries = 1)
+			matches = tool.check(Text) #checks the grammar of the generated text
+			paragraphText += language_check.correct(Text, matches) #corrects any mistakes the grammar checker found in the text
+		Bundle.paragraphs(key) = paragraphText
+		paragraphText = ""
+	return Bundle
+		
+		
+		
 
 
-def TrainAndSave(FILEPATH, FILENAME):
-	corpus = open(FILEPATH).read()
+
+def TrainAndSaveString(String, name):
+	FILENAME  = name
+	os.makedirs(os.path.dirname(FILENAME), exist_ok=True)
+	corpus = String
 	corpus = re.sub( '\s+', ' ', corpus ).strip()
-	text_model = EditedTextClass(corpus, state_size=4, chain=None)
+	text_model = EditedTextClass(corpus, state_size=3, chain=None)
 	model1_json = text_model.to_json()
 	with open(FILENAME, 'w') as outfile:  
 	    json.dump(model1_json, outfile)
