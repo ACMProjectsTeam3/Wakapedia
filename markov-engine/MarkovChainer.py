8352cc17d22e567ceae0f4cf002e7f43c0a98e37
Elizabeth Trinh <etrinh96@gmail.com>
2017-03-31 00:39:54 -0500

    Update MarkovChainer.py

--- markov-engine/MarkovChainer.py
+++ markov-engine/MarkovChainer.py
@@ -9,21 +9,21 @@ import os
 import random
 from random import randint
 
-    
+
 
 def CreateSentences(Bundle): #definition to generate text. Firstword = word.replace(letter,"!") parameter is the file-path to the .txt file you'll be using to train the model, the second parameter is how many sentences you want out of the markov model.
-	FILE_PATH_OF_OLDSTUFF = None 
-	NEW_MODEL = markovify.Text("", state_size = 4) 
+	FILE_PATH_OF_OLDSTUFF = None
+	NEW_MODEL = markovify.Text("", state_size = 4)
 	for x in Bundle.categories:
 		FILE_PATH_OF_OLDSTUFF = x
-		if (FILE_PATH_OF_OLDSTUFF == None):	
-			continue
 		FILE_PATH_OF_OLDSTUFF = FILE_PATH_OF_OLDSTUFF[FILE_PATH_OF_OLDSTUFF.find("Category:") + 9:]
 		FILE_PATH_OF_OLDSTUFF = "Categories/%s.mc" % FILE_PATH_OF_OLDSTUFF
+		if not os.path.isfile(FILE_PATH_OF_OLDSTUFF):
+			continue
 		for letter in FILE_PATH_OF_OLDSTUFF:
 			if letter == '_':
 				FILE_PATH_OF_OLDSTUFF = FILE_PATH_OF_OLDSTUFF.replace(letter, ' ')
-		with open(FILE_PATH_OF_OLDSTUFF) as json_file:  
+		with open(FILE_PATH_OF_OLDSTUFF) as json_file:
 			model2_json = json.load(json_file)
 		OLD_MODEL = markovify.Text.from_json(model2_json)
 		NEW_MODEL = markovify.combine([OLD_MODEL, NEW_MODEL], [1,1])
@@ -33,8 +33,8 @@ def CreateSentences(Bundle): #definition to generate text. Firstword = word.repl
 	for key in Bundle.paragraphs.keys():
 		NumberOfSentences = random.randint(1,12)
 		for i in range(NumberOfSentences): #creates 'NUMSENTENCES' sentence, where NUMSENTENCES is an integer
-			Text = NEW_MODEL.make_sentence(tries = 1) #this, along with the next while loop, basically just forces the markov model to try an infinite number of times to have SOMETHING come out. 
-			while (Text == None): 
+			Text = NEW_MODEL.make_sentence(tries = 1) #this, along with the next while loop, basically just forces the markov model to try an infinite number of times to have SOMETHING come out.
+			while (Text == None):
 				Text = NEW_MODEL.make_sentence(tries = 1)
 			matches = tool.check(Text) #checks the grammar of the generated text
 			paragraphText += language_check.correct(Text, matches) #corrects any mistakes the grammar checker found in the text
@@ -43,8 +43,8 @@ def CreateSentences(Bundle): #definition to generate text. Firstword = word.repl
 		paragraphText = ""
 	return Bundle
 
-		
-		
+
+
 
 def TrainAndSaveString(String, name):
 	FILENAME  = name
@@ -53,5 +53,5 @@ def TrainAndSaveString(String, name):
 	corpus = re.sub( '\s+', ' ', corpus ).strip()
 	text_model = markovify.Text(corpus, state_size=3, chain=None)
 	model1_json = text_model.to_json()
-	with open(FILENAME, 'w') as outfile:  
+	with open(FILENAME, 'w') as outfile:
 	    json.dump(model1_json, outfile)
