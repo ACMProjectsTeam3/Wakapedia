0cb4fa32a04e2a2e197f58402adcfee887c39b1c
Elizabeth Trinh <etrinh96@gmail.com>
2017-03-30 22:01:57 -0500

    Update reconstructor.py

--- reconstructor.py
+++ reconstructor.py
@@ -1,10 +1,8 @@
 import Bundle
 
-object = Bundle()
-
 def reconstruct(b):
     for key in b.paragraphs.keys():
         para = b.paragraphs[key]
-        start = b.html.find[key]
-        b.html = object.html[0:start] + para + object.html[start + len(key) : -1]
-    return object
+        start = b.html.find(key)
+        b.html = b.html[0:start] + para + b.html[start + len(key) : -1]
+    return b
